6984ed93cb898be20cfe09b993d02dc4407ab8a1
Dionísio <dionisioni304@gmail.com>
2025-02-10 18:37:19 +0100

    ...

--- database/migrations/2025_02_10_114243_create_produtos_table.php
+++ database/migrations/2025_02_10_114243_create_produtos_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('stocks', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->id();
-            $table->produto('string');
-            $table->produto('string');
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('stocks');
+        Schema::dropIfExists('produtos');
     }
 };
